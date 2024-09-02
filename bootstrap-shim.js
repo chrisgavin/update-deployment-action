1b7dbed434a5206211a32addab300b4f52bf8857
Chris Gavin <chris@chrisgavin.me>
2024-09-02 10:42:04 +0100

    Update bootstrap shim for ES module format.

--- bootstrap-shim.js
+++ bootstrap-shim.js
@@ -1,20 +1,17 @@
-"use strict";
+import * as childProcess from "child_process";
+import * as fs from "fs";
+import * as path from "path";
 
-const childProcess = require("child_process");
-const fs = require("fs");
-const path = require("path");
-
-const target = path.join(__dirname, "build", "index.js");
+const target = path.join(import.meta.dirname, "build", "index.js");
 try {
 	if (!fs.existsSync(target)) {
-		console.log("::group::This is a development build! For greater reliability and faster startup time, please use a tagged release.");
-		childProcess.execFileSync("npm", ["run", "bootstrap"], {stdio: "inherit", cwd: __dirname});
-		console.log("::endgroup::");
+		console.warn("This is a development build! For greater reliability and faster startup time, please use a tagged release.");
+		childProcess.execFileSync("npm", ["run", "bootstrap"], {stdio: "inherit", cwd: import.meta.dirname});
 	}
 	childProcess.execFileSync(process.execPath, [target], {stdio: "inherit"});
 }
 catch (error) {
-	if (!error.status) {
+	if (error.status === undefined) {
 		throw error;
 	}
 	process.exitCode = error.status;
